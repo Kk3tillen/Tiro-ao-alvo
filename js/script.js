5c65045e5d07a3142112921d62191fce11ba7919
Kk3tillen <k.kezia@academico.ifrn.edu.br>
2023-07-05 22:46:39 -0300

    add style, modificações no js e footer

--- js/script.js
+++ js/script.js
@@ -1,16 +1,31 @@
-var meuComponente = document.getElementById("meuComponente");
-var minhaImagem = meuComponente.querySelector("img");
-var container = document.getElementById("game");
+let meuComponente = document.getElementById("meuComponente");
+let acertou = document.getElementById("acertou");
+let errou = document.getElementById("errou");
+let todo = document.getElementById("todo");
+
+let valor1 = parseInt(acertou.textContent);
+let valor2 = parseInt(errou.textContent); 
+
+let minhaImagem = meuComponente.querySelector("img");
+let container = document.getElementById("game");
 meuComponente.addEventListener("click", moverComponente);
 
 function moverComponente() {
-    var containerWidth = container.clientWidth;
-    var containerHeight = container.clientHeight;
+    let containerWidth = container.clientWidth;
+    let containerHeight = container.clientHeight;
     
-    var novoLeft = Math.random() * (containerWidth - meuComponente.offsetWidth);
-    var novoTop = Math.random() * (containerHeight - meuComponente.offsetHeight);
+    let novoLeft = Math.random() * (containerWidth - meuComponente.offsetWidth);
+    let novoTop = Math.random() * (containerHeight - meuComponente.offsetHeight);
 
     meuComponente.style.left = novoLeft + "px";
     meuComponente.style.top = novoTop + "px";
 
+    acertou.innerHTML = valor1++;
 }
+document.getElementById("game").onclick=function(){
+   errou.innerHTML = valor2++; 
+}
+
+
+
+
