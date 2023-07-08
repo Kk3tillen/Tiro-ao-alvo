9cf3fcf370c41622784591c20f2df1457519f5d4
Kézia Ketillen <88369589+Kk3tillen@users.noreply.github.com>
2023-07-08 21:53:04 +0000

    mudanças no html, js e css

--- js/script.js
+++ js/script.js
@@ -1,31 +1,41 @@
-let meuComponente = document.getElementById("meuComponente");
+const MOVER_COMPONENTE_AUTOMATICO_APOS_SECS = 3000;
+let target = document.getElementById("target");
 let acertou = document.getElementById("acertou");
 let errou = document.getElementById("errou");
 let todo = document.getElementById("todo");
 
-let valor1 = parseInt(acertou.textContent);
-let valor2 = parseInt(errou.textContent); 
+let valor1 = 0;
+let valor2 = 0; 
 
-let minhaImagem = meuComponente.querySelector("img");
+let moverComponenteAutomatico;
+
 let container = document.getElementById("game");
-meuComponente.addEventListener("click", moverComponente);
+target.addEventListener("click", (e) => {
+    e.stopPropagation();
+    acertou.innerHTML = ++valor1;
+    clearInterval(moverComponenteAutomatico);
+    moverComponente();
+    moverComponenteAutomatico = setInterval(moverComponente, MOVER_COMPONENTE_AUTOMATICO_APOS_SECS)
+    todo.innerHTML = valor1 - valor2;
+});
 
 function moverComponente() {
+    const offset = 50;
     let containerWidth = container.clientWidth;
     let containerHeight = container.clientHeight;
     
-    let novoLeft = Math.random() * (containerWidth - meuComponente.offsetWidth);
-    let novoTop = Math.random() * (containerHeight - meuComponente.offsetHeight);
+    let novoLeft = Math.floor(Math.random()*containerWidth-offset);
+    let novoTop = Math.floor(Math.random() * containerHeight-offset);
+    
+    const flip = Math.floor(Math.random()*2);
+    target.style.transform= (flip)? 'scale(-1,1)' : 'scale(1,1)';
 
-    meuComponente.style.left = novoLeft + "px";
-    meuComponente.style.top = novoTop + "px";
-
-    acertou.innerHTML = valor1++;
+    target.style.left = novoLeft + "px";
+    target.style.top = novoTop + "px";
 }
+
+moverComponenteAutomatico = setInterval(moverComponente, MOVER_COMPONENTE_AUTOMATICO_APOS_SECS);
 document.getElementById("game").onclick=function(){
-   errou.innerHTML = valor2++; 
+   errou.innerHTML = ++valor2; 
+   todo.innerHTML = valor1 - valor2;
 }
-
-
-
-
